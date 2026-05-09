11d0b0d19bf4980a11c356a5eac93067a70f8aca
Mauricio Míguez <124924063+JertsM@users.noreply.github.com>
2026-05-09 22:40:08 +0200

    Creación del proyecto con Spring Boot

---
+++ dam/src/test/kotlin/com/clinicodi/dam/DamApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.clinicodi.dam
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DamApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
